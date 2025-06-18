9a57fbd8303f77a69e0fb13d547889ae79f271f2
raphaelkottakal2 <raphael.kottakal2@swiggy.in>
2025-06-18 16:07:37 +0530

    imlement rotation

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { PivotControls, useGLTF } from "@react-three/drei";
-import { useMemo, useRef, useEffect } from "react";
-import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+import { useRef, useEffect } from "react";
+import { RigidBody } from "@react-three/rapier";
 import { DeviceOrientationControls } from "three-stdlib";
 import { useFrame } from "@react-three/fiber";
 
@@ -10,90 +10,47 @@ export default function () {
     "https://instamart-media-assets.swiggy.com/DeSo/test/fiber-testing/Player.glb?updatedAt=1747315104926"
   );
   const playerRef = useRef(null);
-  const mainMesh = useMemo(() => {
-    if (playerRef.current) {
-      console.log("playerRef", playerRef);
-
-      playerRef.current.material.transparent = true;
-      playerRef.current.material.opaity = true;
-      playerRef.current.material.needsUpdate = true;
-      // playerRef.current.material.opacity = 0.5;
-    }
+  useEffect(() => {
     const mesh = scene.getObjectByName("Player");
     if (!mesh) {
       console.error("Player mesh not found in the loaded model");
     }
     mesh.castShadow = true;
     mesh.receiveShadow = true;
-    // console.log("mesh", mesh);
-    return mesh;
   }, [scene, playerRef.current]);
 
-  const originEuler = useRef(new THREE.Euler(0, 0, 0));
-  const quaternionX = useRef(new THREE.Quaternion());
-  const vectorX = useRef(new THREE.Vector3(1, 0, 0).normalize());
-  const quaternionY = useRef(new THREE.Quaternion());
-  const vectorY = useRef(new THREE.Vector3(0, 1, 0).normalize());
-  const quaternionZ = useRef(new THREE.Quaternion());
-  const vectorZ = useRef(new THREE.Vector3(0, 0, 1).normalize());
-  const prevRef = useRef({
-    prevAlpha: 0,
-    prevBeta: 0,
-    prevGamma: 0,
-  });
-  const rotationIsReady = useRef(false);
+  const lineRef = useRef();
+  const lookAtRef = useRef(new THREE.Vector3(0, 0, 1));
+  const zee = useRef(new THREE.Vector3(0, 0, 1));
+  const anEuler = useRef(new THREE.Euler());
+  const q0 = useRef(new THREE.Quaternion());
+  const q1 = useRef(
+    new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5))
+  );
 
   const controls = useRef(null);
 
   useEffect(() => {
-    controls.current = new DeviceOrientationControls(playerRef.current);
-    controls.current.alphaOffset = -Math.PI / 2;
+    // controls.current = new DeviceOrientationControls(playerRef.current);
+    // controls.current.alphaOffset = -Math.PI / 2;
     const handelOrientationEvent = (event) => {
       const { alpha, beta, gamma } = event;
-      const { prevAlpha, prevBeta, prevGamma } = prevRef.current;
-      // console.log("controls", controls.current.euler);
-      // console.log("player", playerRef.current.rotation);
-      // console.log("orientationValues", beta, playerRef.current);
+      const radBeta = THREE.MathUtils.degToRad(beta);
+      const radGama = THREE.MathUtils.degToRad(gamma);
+      const radAlpha = THREE.MathUtils.degToRad(alpha);
 
-      const radX = THREE.MathUtils.degToRad(beta);
-      const radY = THREE.MathUtils.degToRad(gamma);
-      const radZ = THREE.MathUtils.degToRad(alpha);
-      // playerRef.current.rotation.set(radX - Math.PI / 2, 0, 0);
+      anEuler.current.set(radBeta, radAlpha, -radGama, "YXZ"); // 'ZXY' for the device, but 'YXZ' for us
+      playerRef.current.quaternion.setFromEuler(anEuler.current); // orient the device
+      playerRef.current.quaternion.multiply(q1.current); // camera looks out the back of the device, not the top
+      playerRef.current.quaternion.multiply(
+        q0.current.setFromAxisAngle(zee.current, 0)
+      ); // adjust for screen orientation
 
-      // playerRef.current.rotation.copy(origin/Euler.current);
+      // const lookAtVector = new THREE.Vector3(0, 0, -1);
+      lookAtRef.current.set(0, 0, 1);
+      lookAtRef.current.applyQuaternion(playerRef.current.quaternion);
 
-      // console.log(playerRef.current.rotation.x);
-
-      const radXdiff = playerRef.current.rotation.x - radX + Math.PI / 2;
-      const radYdiff = playerRef.current.rotation.y - radY;
-      const radZdiff = playerRef.current.rotation.z - radZ;
-
-      // quaternionX.current.setFromAxisAngle(vectorX.current, radXdiff);
-      // playerRef.current.applyQuaternion(quaternionX.current);
-
-      // quaternionY.current.setFromAxisAngle(vectorY.current, radYdiff);
-      // playerRef.current.applyQuaternion(quaternionY.current);
-
-      // quaternionZ.current.setFromAxisAngle(vectorZ.current, radZdiff);
-      // playerRef.current.applyQuaternion(quaternionZ.current);
-
-      // quaternionY.current.setFromAxisAngle(
-      //   vectorY.current,
-      //   THREE.MathUtils.degToRad(gamma - prevGamma)
-      // );
-      // playerRef.current.applyQuaternion(quaternionY.current);
-
-      // quaternionZ.current.setFromAxisAngle(
-      //   vectorZ.current,
-      //   THREE.MathUtils.degToRad(alpha - prevAlpha)
-      // );
-      // playerRef.current.applyQuaternion(quaternionY.current);
-
-      prevRef.current = {
-        prevAlpha: alpha,
-        prevBeta: beta,
-        prevGamma: gamma,
-      };
+      lookAtRef.current.multiplyScalar(3);
     };
     addEventListener("deviceorientation", handelOrientationEvent);
 
@@ -102,8 +59,22 @@ export default function () {
     };
   }, []);
   useFrame(() => {
-    controls.current.update();
+    const points = [];
+    points.push(new THREE.Vector3());
+    points.push(lookAtRef.current);
+
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    lineRef.current.geometry = lineGeometry;
+
+    // controls.current.update();
   });
+
+  const points = [];
+  points.push(new THREE.Vector3());
+  points.push(lookAtRef.current);
+
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
   return (
     <>
       <RigidBody type="fixed" colliders="hull">
@@ -116,9 +87,18 @@ export default function () {
             name="main-player"
             ref={playerRef}
             scale={[1, 1, 1]}
-            object={mainMesh}
+            object={scene}
           />
         </group>
+        <line ref={lineRef} geometry={lineGeometry}>
+          <lineBasicMaterial
+            attach="material"
+            color={"#ff0000"}
+            linewidth={10}
+            linecap={"round"}
+            linejoin={"round"}
+          />
+        </line>
       </RigidBody>
     </>
   );
